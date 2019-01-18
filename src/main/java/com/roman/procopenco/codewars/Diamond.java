448066742423940522af8d0183a9dd4a5af333f8
Roman Procopenco <procopenco.roman@gmail.com>
2019-01-18 23:39:46 +0100

    Remove ignored files

--- src/main/java/com/roman/procopenco/codewars/Diamond.java
+++ src/main/java/com/roman/procopenco/codewars/Diamond.java
@@ -1,17 +1,49 @@
 package com.roman.procopenco.codewars;
 
 
+import java.util.Collections;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static java.lang.String.join;
+import static java.util.Collections.nCopies;
+
 public class Diamond {
 
     /**
      * https://www.codewars.com/kata/give-me-a-diamond/train/java
+     *
      * @param n
      * @return
      */
 
     public static String print(int n) {
-        if (n < 0 || Math.floorMod(n, 2) == 0 ) return null;
+        if (n < 0 || Math.floorMod(n, 2) == 0) return null;
+        StringBuilder sb = new StringBuilder();
+        int spaces = n / 2;
+        int asterisks = 1;
+        for (int i = 1; i <= n; i++) {
+            sb.append(join("", nCopies(spaces, " ")));
+            sb.append(join("", nCopies(asterisks, "*")));
+            sb.append("\n");
+            if (i <= n / 2) {
+                spaces = spaces - 1;
+                asterisks = asterisks + 2;
+            } else {
+                spaces = spaces + 1;
+                asterisks = asterisks - 2;
+            }
+        }
 
-        return null;
+        return sb.toString();
+    }
+
+    public static String printStream(int n) {
+        return n < 0 || n % 2 == 0 ? null : IntStream
+                .range(0, n)
+                .mapToObj(x ->
+                        join("", nCopies(Math.abs(n / 2 - x), " ")) +
+                                join("", nCopies(n - 2 * Math.abs(n / 2 - x), "*")))
+                .collect(Collectors.joining("\n")) + "\n";
     }
 }
