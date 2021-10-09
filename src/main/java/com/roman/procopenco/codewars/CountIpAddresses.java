0e7807a2bf4fbb651c55f67cc7931c53427738e9
Roomka <romaproc@gmail.com>
2021-10-09 12:52:05 +0200

    Added CountIpAddresses problem description

--- src/main/java/com/roman/procopenco/codewars/CountIpAddresses.java
+++ src/main/java/com/roman/procopenco/codewars/CountIpAddresses.java
@@ -1,5 +1,18 @@
 package com.roman.procopenco.codewars;
 
+/**
+ * Implement a function that receives two IPv4 addresses, and returns the number of addresses between them (including the first one, excluding the last one).
+ *
+ * All inputs will be valid IPv4 addresses in the form of strings. The last address will always be greater than the first one.
+ *
+ * Examples
+ * ips_between("10.0.0.0", "10.0.0.50")  ==   50
+ * ips_between("10.0.0.0", "10.0.1.0")   ==  256
+ * ips_between("20.0.0.10", "20.0.1.0")  ==  246
+ *
+ * https://www.codewars.com/kata/526989a41034285187000de4/train/java
+ */
+
 public class CountIpAddresses {
   public static long ipsBetween(String start, String end) {
 
